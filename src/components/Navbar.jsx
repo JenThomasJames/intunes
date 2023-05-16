5c9371b735a352893b3c28f0f54a4e3e81a11db9
JenThomasJames <jenthomasjames123@gmail.com>
2023-05-16 23:47:08 +0530

    Fix: Remove Downloader button from navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,8 @@ import Button from "./Button";
 
 const Navbar = () => {
   return (
-    <div className="min-w-full py-3 px-6 flex justify-between items-center">
+    <div className="min-w-full py-3 px-6">
       <img className="w-28 cursor-pointer" src={Logo} alt="logo" />
-      <ul className="flex gap-5">
-        <li>
-          <Button>Downloader</Button>
-        </li>
-      </ul>
     </div>
   );
 };
