d79c296fcb50ee8b6bab18244ab076398ae12798
JenThomasJames <jenthomasjames123@gmail.com>
2023-05-17 10:44:46 +0530

    Feat: Render the navbar and downloader in the app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Downloader from "./components/Downloader";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return;
+  return (
+    <div className="min-w-full min-h-screen flex flex-col">
+      <Navbar />
+      <Downloader />
+    </div>
+  );
 }
 
 export default App;
