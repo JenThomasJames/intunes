e28eba555823a010d06b570063680dac16d17f20
JenThomasJames <jenthomasjames123@gmail.com>
2023-05-17 10:43:30 +0530

    Feat: Make the button component responsive and add contained variant for button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 
-const Button = ({ variant = "text", children, onClick }) => {
+const Button = ({
+  variant = "contained",
+  children,
+  onClick,
+  type = "button",
+}) => {
   if (variant === "text") {
     return (
       <button
-        className="text-slate-400 text-lg hover:text-teal-500"
+        className="text-slate-400 text-xs sm:text-sm md:text-md lg:text-base hover:text-teal-500"
         onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
+    );
+  }
+  if (variant === "contained") {
+    return (
+      <button
+        className="bg-teal-500 flex justify-center items-center flex-0 min-w-fit py-2 px-3 sm:px-4 md:px-5 text-xs sm:text-sm md:text-md lg:px-5 font-semibold  text-teal-100 rounded hover:bg-teal-600 hover:shadow-md"
+        onClick={onClick}
+        type={type}
       >
         {children}
       </button>
