fab2396a26b3f8ccc67ac221ac7822d424f34e6e
JenThomasJames <jenthomasjames123@gmail.com>
2023-05-17 12:53:39 +0530

    Feat: Add icon button support for button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -20,7 +20,18 @@ const Button = ({
   if (variant === "contained") {
     return (
       <button
-        className="bg-teal-500 flex justify-center items-center flex-0 min-w-fit py-2 px-3 sm:px-4 md:px-5 text-xs sm:text-sm md:text-md lg:px-5 font-semibold  text-teal-100 rounded hover:bg-teal-600 hover:shadow-md"
+        className="bg-teal-500 flex justify-center items-center flex-0 min-w-fit py-3 px-3 sm:px-4 md:px-5 text-xs sm:text-sm md:text-md lg:px-5 font-semibold  text-teal-100 rounded hover:bg-teal-600 hover:shadow-md"
+        onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
+    );
+  }
+  if (variant === "icon") {
+    return (
+      <button
+        className="bg-transparent hover:bg-red-300 p-2 flex justify-center items-center flex-0 min-w-fit text-xs sm:text-sm md:text-md font-semibold text-teal-100 rounded-full"
         onClick={onClick}
         type={type}
       >
